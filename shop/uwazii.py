ea391fb97f14e73f87f5ba28bf1b7f5b91d03fab
dennis nyaga <murildennis7@gmail.com>
2024-11-12 08:41:36 +0300

    added smsk

--- shop/uwazii.py
+++ shop/uwazii.py
@@ -1,8 +1,7 @@
 import requests
 from datetime import datetime
 
-# Replace this with your actual access token
-ACCESS_TOKEN = 'aa4ce93cf54e5335631812f13e0a2ce1'
+
 API_URL = 'https://restapi.uwaziimobile.com/v1/send'
 
 def format_phone_number(phone_number):
